43330a334e7096aa45762e13a491e5c919de7445
ankush850 <ankushsinghrawat154@gmail.com>
2023-12-21 12:09:37 +0530

    Set up aiohttp / httpx async client session management

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        ink: "#0d0f14",
+        steel: "#1b1f2a",
+        ember: "#ff6b4a",
+        emberSoft: "#ffb199",
+        sky: "#4fb0ff",
+        mint: "#43f2b1",
+      },
+      boxShadow: {
+        glow: "0 0 30px rgba(255, 107, 74, 0.2)",
+      },
+    },
+  },
+  plugins: [],
+};
